aea26f49ea3b320a306c5d764dd2150076bc8923
Leandro Amorim <leamorim@outlook.com>
2024-05-30 17:17:33 -0400

    Add project files.

---
+++ src/Infrastructure/Data/Configurations/TodoItemConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Assignment.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Assignment.Infrastructure.Data.Configurations;
+
+public class TodoItemConfiguration : IEntityTypeConfiguration<TodoItem>
+{
+    public void Configure(EntityTypeBuilder<TodoItem> builder)
+    {
+        builder.Property(t => t.Title)
+            .HasMaxLength(200)
+            .IsRequired();
+
+        builder.HasIndex(t => t.Title)
+            .IsUnique();
+    }
+}
